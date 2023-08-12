a79d14f0a3b49f209c44c46270e100412596e175
TvoyKot <a.lshe@yandex.by>
2023-08-12 11:31:35 +0300

    continue

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,19 @@
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/svgElements/arrow-left.svg" alt="arrow"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/svgElements/arrow-right.svg" alt="arrow"></button>'
+    })
 
+    $('.search__tabs-item').on('click', function(e) {
+        e.preventDefault();
+
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+
+        $(this).addClass('search__tabs-item--active');
+
+        $($(this).attr('href')).addClass('search__content-item--active');
+    });
+});
 
